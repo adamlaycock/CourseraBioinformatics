f7c037e803bb0654a70b6a93a6db5c0a054bdf0a
adamlaycock <adam.m.laycock@gmail.com>
2025-05-28 18:22:56 +0100

    change file structure and add V. cholerae exercise

---
+++ Week 1/Exercises/DnaA Box Finder.py
@@ -0,0 +1,22 @@
+def find_patterns(text: str, pattern: str) -> list:
+    """_summary_
+
+    Args:
+        text (str): Text in which patterns are searched for.
+        pattern (str): Pattern to find.
+
+    Returns:
+        list: List of indices where a pattern begins in the text.
+    """
+    starts = []
+    for i in range(len(text) - len(pattern) + 1):
+        if text[i:i + len(pattern)] == pattern:
+            starts.append(i)
+
+    return starts
+
+with open('Vibrio_cholerae.txt', 'r') as file:
+    text = file.read()
+pattern = 'CTTGATCAT'
+
+print(*find_patterns(text, pattern))
