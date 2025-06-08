567caa725d4878b98be14fbcba75a05f6aca46b7
adamlaycock <adam.m.laycock@gmail.com>
2025-06-08 14:50:27 +0100

    update find clumps and add optimised find clumps for e. coli genome

---
+++ Week 1/Exercises/E. coli Genome/FindClumps.py
@@ -0,0 +1,50 @@
+def FindClumps(text: str, k: int, L: int, t: int):
+    """_summary_
+
+    Args:
+        text (str): Text in which clumps are searched for.
+        k (int): Length of the kmers to be found.
+        L (int): Length of the window to look for kmers in.
+        t (int): Number of times a kmer must appear in a window to be logged.
+
+    Returns:
+        patterns: kmers which appear t times in some window of length L.
+    """
+    from collections import defaultdict
+
+    n = len(text)
+    clumps = set()
+    kmer_counts = defaultdict(int)
+
+    # First window
+    for i in range(L - k + 1):
+        kmer = text[i:i+k]
+        kmer_counts[kmer] += 1
+
+    for kmer, count in kmer_counts.items():
+        if count >= t:
+            clumps.add(kmer)
+
+    # Slide the window
+    for i in range(1, n - L + 1):
+        # Remove k-mer that slides out
+        first_kmer = text[i-1:i-1+k]
+        kmer_counts[first_kmer] -= 1
+
+        # Add new k-mer that comes in
+        new_kmer = text[i + L - k:i + L]
+        kmer_counts[new_kmer] += 1
+
+        if kmer_counts[new_kmer] >= t:
+            clumps.add(new_kmer)
+
+    return list(clumps)
+
+with open('E_coli.txt', 'r') as file:
+    text = file.read()
+
+k = 9
+L = 500
+t = 3
+
+print(len(FindClumps(text, k, L, t)))
--- Week 1/Functions/FindClumps/FindClumps.py
+++ Week 1/Functions/FindClumps/FindClumps.py
@@ -1,4 +1,15 @@
 def FindClumps(text: str, k: int, L: int, t:int):
+    """_summary_
+
+    Args:
+        text (str): Text in which clumps are searched for.
+        k (int): Length of the kmers to be found.
+        L (int): Length of the window to look for kmers in.
+        t (int): Number of times a kmer must appear in a window to be logged.
+
+    Returns:
+        patterns: kmers which appear t times in some window of length L.
+    """
     patterns = set()
     for i in range(len(text) - L + 1):
         window = text[i:i + L]
@@ -8,3 +19,12 @@ def FindClumps(text: str, k: int, L: int, t:int):
             pattern_counts[pattern] = pattern_counts.get(pattern, 0) + 1
         patterns.update([p for p, c in pattern_counts.items() if c >= t])
     return list(patterns)
+
+with open('dataset_30274_5.txt', 'r') as file:
+    text = file.read()
+
+k = 10
+L = 100
+t = 4
+
+print(*FindClumps(text, k, L, t))
